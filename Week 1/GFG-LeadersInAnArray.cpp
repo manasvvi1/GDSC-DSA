2e59ac0546f9207b907612918e019388e7742d71
Manasvvi <manasvvi.aggarwal@gmail.com>
2021-10-05 17:20:45 +0530

    updated Week 1 assignment

--- Week 1/GFG-LeadersInAnArray.cpp
+++ Week 1/GFG-LeadersInAnArray.cpp
@@ -27,3 +27,30 @@ class Solution{
         return leaders;
     }
 };
+
+//  correct answer
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        vector<int> revArr;
+        int maximum = a[n-1];
+        
+        revArr.push_back(a[n-1]);
+        for(int i = n - 2 ; i >= 0 ; i--){
+            if(maximum <= a[i]){
+                maximum = a[i];
+                revArr.push_back(a[i]);
+            }
+        }
+        
+        vector<int> arr;
+        
+        for(int i = revArr.size() - 1 ; i >= 0 ; i--){
+            arr.push_back(revArr[i]);
+        }
+        
+        return arr;
+    }
+};
--- Week 1/Leetcode-FindPivotIndex.cpp
+++ Week 1/Leetcode-FindPivotIndex.cpp
@@ -1,7 +1,5 @@
 //  https://leetcode.com/problems/find-pivot-index/
 
-//  TLE received
-
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
@@ -9,26 +7,47 @@ public:
         int sum[n], count=0;
         
         for(int i = 0 ; i < n ; i++){
-            count += sum[i];
+            count += nums[i];
             sum[i] = count;
         }
         
         for(int i = 0 ; i < n ; i++){
-            if(i == 0){
-                if(sum[n-1] == 0){
+            if((i == 0) && ((sum[n-1]-sum[0]) == 0)){
                     return 0;
-                }
             }
-            else if(i == (n-1)){
-                if(sum[n-2] == 0){
+            else if((i == (n-1)) && (sum[n-2] == 0)){
                     return n-1;
-                }
             }
-            else{
-                if(sum[i-1] == (sum[n-1] - sum[i+1])){
+            else if((i>0) && (i<(n-1))){
+                if(sum[i-1] == (sum[n-1] - sum[i])){
                     return i;
                 }
             }
+        }
+        
+        return -1;
+    }
+};
+
+// another solution
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left = 0, right = 0;
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            right += nums[i];
+        }
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            right -= nums[i];
+            
+            if(left == right){
+                return i;
+            }
+            
+            left += nums[i];
         }
         
         return -1;
